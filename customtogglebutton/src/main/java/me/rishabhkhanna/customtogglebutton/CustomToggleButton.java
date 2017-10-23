cbe222b3e9693217fb4c4f28eaf152140fdc367c
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-10-24 00:55:54 +0530

    getting all attributes and applying
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- customtogglebutton/src/main/java/me/rishabhkhanna/customtogglebutton/CustomToggleButton.java
+++ customtogglebutton/src/main/java/me/rishabhkhanna/customtogglebutton/CustomToggleButton.java
@@ -1,8 +1,10 @@
 package me.rishabhkhanna.customtogglebutton;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
 import android.content.res.TypedArray;
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.DrawableContainer;
 import android.graphics.drawable.GradientDrawable;
@@ -15,6 +17,7 @@ import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.FrameLayout;
@@ -25,9 +28,10 @@ import android.widget.ToggleButton;
  * Created by rishabhkhanna on 23/09/17.
  */
 
-public class CustomToggleButton extends ToggleButton{
+public class CustomToggleButton extends ToggleButton {
 
     public static final String TAG = "CustomToggleButton";
+
     public CustomToggleButton(Context context) {
         super(context);
         initialize(context);
@@ -35,12 +39,12 @@ public class CustomToggleButton extends ToggleButton{
 
     public CustomToggleButton(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        initialize(context,attrs) ;
+        initialize(context, attrs);
     }
 
     public CustomToggleButton(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        initialize(context,attrs);
+        initialize(context, attrs);
     }
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
@@ -49,33 +53,56 @@ public class CustomToggleButton extends ToggleButton{
         initialize(context);
     }
 
-    public void initialize(Context context){
+    public void initialize(Context context) {
 
-        LayoutInflater.from(context).inflate(R.layout.layout,null,false);
+        LayoutInflater.from(context).inflate(R.layout.layout, null, false);
     }
 
-    public void initialize(Context context, AttributeSet attrs){
+    public void initialize(Context context, AttributeSet attrs) {
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            this.setBackground(getResources().getDrawable(R.drawable.toggle_background,null));
-        }else {
+            this.setBackground(getResources().getDrawable(R.drawable.toggle_background, null));
+        } else {
             this.setBackground(getResources().getDrawable(R.drawable.toggle_background));
         }
+
         StateListDrawable stateListDrawable = (StateListDrawable) this.getBackground();
         DrawableContainer.DrawableContainerState dcs = (DrawableContainer.DrawableContainerState) stateListDrawable.getConstantState();
         Drawable[] drawableItems = dcs.getChildren();
-        GradientDrawable gradientDrawableUnChecked = (GradientDrawable) drawableItems[0];
-        GradientDrawable gradientDrawablechecked = (GradientDrawable) drawableItems[1];
-        TypedArray typedArray = context.obtainStyledAttributes(attrs,R.styleable.CustomToggleButton);
-//        getting all the attributes values set from the user
-        int toggleOnColor = typedArray.getColor(R.styleable.CustomToggleButton_toogleOnColor, Color.parseColor("#FF4081"));
-        int toggleOffColor = typedArray.getColor(R.styleable.CustomToggleButton_toggleOffColor, Color.parseColor("#FF4081"));
+        GradientDrawable unChecked = (GradientDrawable) drawableItems[0];
+        GradientDrawable checked = (GradientDrawable) drawableItems[1];
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.CustomToggleButton);
+
+//        getting all the attributes values set from the typed array i.e from user
+        int toggleOnColor = typedArray.getColor(R.styleable.CustomToggleButton_checkedColor, Color.parseColor("#FF4081"));
+        int toggleOffColor = typedArray.getColor(R.styleable.CustomToggleButton_uncheckedColor, Color.parseColor("#FF4081"));
+        float borderWidth = typedArray.getDimension(R.styleable.CustomToggleButton_borderWidth, 4.0f);
+        float radius = typedArray.getDimension(R.styleable.CustomToggleButton_radius, 15.0f);
+        int checkedTextColor = typedArray.getColor(R.styleable.CustomToggleButton_checkedTextColor, getResources().getColor(R.color.CheckedTextColor));
+        int uncheckedTextColor = typedArray.getColor(R.styleable.CustomToggleButton_uncheckedTextColor, getResources().getColor(R.color.uncheckedTextColor));
+        Log.d(TAG, "initialize: " + borderWidth);
+        ColorStateList colorStateList = new ColorStateList(
+                new int[][]{
+                        new int[]{android.R.attr.state_checked},
+                        new int[]{-android.R.attr.state_checked}
+                },
+                new int[]{
+                        checkedTextColor,
+                        uncheckedTextColor
+                }
+        );
+
+        this.setTextColor(colorStateList);
 
 
-        Float borderCheckedWidth = typedArray.getDimension(R.styleable.CustomToggleButton_borderWidth, 4.0f);
-        gradientDrawableUnChecked.setStroke(Math.round(borderCheckedWidth),toggleOnColor);
-        gradientDrawablechecked.setColor(toggleOffColor);
-        gradientDrawablechecked.setStroke(Math.round(borderCheckedWidth),toggleOffColor);
+        checked.setStroke(Math.round(borderWidth), toggleOnColor);
+        checked.setColor(toggleOnColor);
+        checked.setCornerRadius(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, radius, getResources().getDisplayMetrics()));
+
+
+        unChecked.setStroke(Math.round(borderWidth), toggleOffColor);
+        unChecked.setCornerRadius(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, radius, getResources().getDisplayMetrics()));
+
 
 
     }
